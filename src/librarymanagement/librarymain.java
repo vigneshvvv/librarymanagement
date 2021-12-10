6ac14d428d3b1a9841336e24994d69c619897c03
vigneshvvv <vignesh33319@gmail.com>
2021-12-10 09:05:10 +0530

    libraryprogram

---
+++ src/librarymanagement/librarymain.java
@@ -0,0 +1,43 @@
+package librarymanagement;
+
+public class librarymain {
+	
+	private int id;
+	private String bookname;
+	private String authorinformation;
+	private int date;
+	private int price;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getBookname() {
+		return bookname;
+	}
+	public void setBookname(String bookname) {
+		this.bookname = bookname;
+	}
+	public String getAuthorinformation() {
+		return authorinformation;
+	}
+	public void setAuthorinformation(String authorinformation) {
+		this.authorinformation = authorinformation;
+	}
+	public int getDate() {
+		return date;
+	}
+	public void setDate(int date) {
+		this.date = date;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
+
+}
